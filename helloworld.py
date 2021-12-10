8cb00eab395c8838297e10d673244218f8d54e07
cheng.chen <cheng.chen@hachibot.com>
2021-12-10 19:38:18 +0800

    person say something

--- helloworld.py
+++ helloworld.py
@@ -12,3 +12,8 @@ person2.speak('Hi, I am person2')
 person1.speak('nice to meet you')
 
 person2.speak('nice to meet you too')
+
+
+person1.speak('where are you from')
+
+person2.speak('I am from China')
